4994539c8e23f134c37699ee6336c59e42d4f4ac
Anu Raife <anuradharaife2020@u.northwestern.edu>
2019-09-27 19:44:15 -0400

    update settings

--- hopeanomics/hopeanomics/settings.py
+++ hopeanomics/hopeanomics/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dg1*7yiilg-w@@#i*_1i!blzmeftd2lb(r$93fpme8w+v(b2=-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
