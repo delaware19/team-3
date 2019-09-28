939e9b80e5e92377a5192b90c8e5e236b3494f29
khalil0525 <collinskhalil@hotmail.com>
2019-09-28 08:02:22 -0400

    Added "issue" url

--- hopeanomics/rise/urls.py
+++ hopeanomics/rise/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='rise-index'),
+    path('issue.html', views.issue, name='rise-issue'),
     path('choice.html/',  views.choice, name='rise-choice'),
     path('action.html/', views.action, name='rise-action'),
     path('learn.html/', views.learn, name='rise-learn'),
