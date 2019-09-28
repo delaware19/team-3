fb8c07c7ece11b7341d453f12f1dac86b2f81e07
= <=>
2019-09-27 21:23:40 -0400

    Defined initial views

--- hopeanomics/rise/views.py
+++ hopeanomics/rise/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def start(request):
+    return HttpResponse('<h1>Enter your state and campaign</h1>')
+
+def choice(request):
+    return HttpResponse('<h1>Take Action or Learn</h1>')
+
+def action(request):
+    return HttpResponse('<h1>How to Take Action</h1>')
+
+def learn(request):
+    return HttpResponse('<h1>This is your info</h1>')
