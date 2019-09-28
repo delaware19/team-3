04198c5d6082d976bdceea32113f72fa6553420e
Anu Raife <anuradharaife2020@u.northwestern.edu>
2019-09-28 06:01:11 -0400

    fix indenting

--- hopeanomics/rise/models.py
+++ hopeanomics/rise/models.py
@@ -4,12 +4,12 @@ class State(models.Model):
     name = models.CharField(max_length=2, blank=False, null=False, primary_key=True)
 
 
- class Campaign(models.Model):
+class Campaign(models.Model):
     name = models.CharField(max_length=200, blank=False, null=False)
     state = models.ForeignKey("State", models.DO_NOTHING, blank=False, null=False)
 
 
- class Person(models.Model):
+class Person(models.Model):
     first_name = models.CharField(max_length=100, blank=False, null=False)
     last_name = models.CharField(max_length=100, blank=False, null=False)
     email = models.CharField(max_length=300, blank=True, null=True)
@@ -22,7 +22,7 @@ class State(models.Model):
     number = models.BigIntegerField(null=True)
 
 
- class Deadline(models.Model):
+class Deadline(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False, primary_key=True)
     date = models.DateField(blank=False, null=False)
     state = models.ForeignKey("State", models.DO_NOTHING, blank=False, null=False)
